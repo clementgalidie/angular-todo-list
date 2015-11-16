7ee7df392011ea4c61f55e1203b4d081ecc543e5
Clement Galidie <galidie.clement@gmail.com>
2015-11-17 00:14:53 +0100

    reindent JS code plus add 'addTask' method

--- app/todo.js
+++ app/todo.js
@@ -1,17 +1,26 @@
 angular.module('Todo', [])
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function($scope) {
     $scope.tasks = [
       {
-        "id": 0,
-        "title": "First task",
-        "description": "Description of the first task.",
-        "done": false
+        id: 0,
+        title: "First task",
+        description: "Description of the first task.",
+        done: false
       },
       {
-        "id": 1,
-        "title": "Second task",
-        "description": "Description of the second task.",
-        "done": false
+        id: 1,
+        title: "Second task",
+        description: "Description of the second task.",
+        done: false
       }
-    ]
+    ];
+
+    // Add a task.
+    $scope.addTask = function addTask() {
+      $scope.tasks.push({
+        title: $scope.task.title,
+        description: $scope.task.desc,
+        done: false
+      });
+    };
   });
